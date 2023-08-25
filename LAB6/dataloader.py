b45d2270637edc6dd7ea2c5225ab2a53cb109ca4
楊奕儒 <98101443+ben25000233@users.noreply.github.com>
2023-08-25 21:18:03 +0800

    Add files via upload

---
+++ LAB6/dataloader.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import json 
+
+def get_label():
+    root = os.os. getcwd()
+    print(root)
+
+def main():
+    get_label()
+    
+if __name__ == '__main__':
+    main()
